af85eb61fe4e24467efad632f7e76f6a9ae95fd0
NyLaurent <laulanyumbayire@gmail.com>
2026-03-02 18:23:26 -0600

    initial commit

---
+++ vehicle_ml_dashboard/vehicles/views.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from django.shortcuts import render
+from .dashboard import frequency_table
+
+def dashboard_view(request):
+    queryset = pd.read_csv("dummy_data/vehicles_data_1000.csv")
+    df = pd.DataFrame(queryset)
+
+    return render(request, "vehicles/index.html", {
+        "frequency_table": frequency_table(df)
+    })
